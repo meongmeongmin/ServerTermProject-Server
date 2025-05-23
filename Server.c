2a87b9846492eac442b80d2e9418d236df7d71b1
멍멍민 <pr620718@cu.ac.kr>
2025-05-23 13:45:58 +0900

    🚀 feat: 게임 시작 준비
    - 인원 수가 모두 모일 때까지(2명) 대기
     - 현재는 Test를 위해 1명만 모여도 시작 가능

--- Server.c
+++ Server.c
@@ -10,8 +10,7 @@
 
 typedef struct Card
 {
-    /// @brief 카드가 짝 맞춰져 사라졌는지 여부
-    bool matched;
+    bool matched;   // 카드가 짝 맞춰져 사라졌는지 여부
     char name[20];
 } Card;
 
@@ -30,7 +29,47 @@ typedef struct ClientInfo
 
 SOCKET g_server_socket;
 ClientInfo* g_clients[MAX_CLIENTS];
+int g_clientCount = 0;
+
+volatile bool g_startGame = false;  // 게임 시작 여부
 Card g_cards[MAX_CARD_COUNT];
+
+DWORD WINAPI WaitForGameStart(LPVOID arg)
+{
+    printf("WaitForGameStart\n");
+    while (true)
+    {
+        Sleep(1000);
+
+        if (g_startGame)
+            continue;
+        
+        // 인원이 2명 모였는지 확인
+        // if (g_clientCount != 2)
+        // {
+        //     g_startGame = false;
+        //     continue;
+        // }
+
+        // Test
+        if (g_clientCount <= 0)
+        {
+            g_startGame = false;
+            continue;
+        }
+
+        // 먼저 시작할 플레이어 정하기
+        // srand(time(NULL));
+        // int idx = rand() % MAX_CLIENTS; // (0 ~ 1) 정수 범위
+        // g_clients[idx]->player.myTurn = true;
+
+        // Test
+        g_clients[0]->player.myTurn = true;
+        g_startGame = true;
+    }
+
+    ExitThread(0);
+}
 
 void Init()
 {
@@ -73,22 +112,72 @@ void Init()
     }
 
     printf("Echo server running on port %d\n", PORT);
+
+    // 게임 시작 대기 스레드 생성
+    HANDLE thread_id = CreateThread(NULL, 0, WaitForGameStart, NULL, 0, NULL);
+    if (thread_id == NULL)
+    {
+        perror("CreateThread() failed");
+        CloseHandle(thread_id);
+        exit(1);
+    }
+}
+
+void PlayGame(LPVOID arg)
+{
+    ClientInfo* info = (ClientInfo*)arg;
+    printf("%d: PlayGame\n", ntohs(info->addr.sin_port));
+
+    while (true)
+    {
+        Sleep(1000);
+
+        if (g_startGame == false)
+            break;
+        
+        if (info->player.myTurn == false)
+            continue;
+
+        // TODO
+
+        printf("Test!\n");
+        break;
+    }
+}
+
+void ExitGame(LPVOID arg)
+{
+    ClientInfo* info = (ClientInfo*)arg;
+    printf("%d: ExitGame\n", ntohs(info->addr.sin_port));
+
+    g_clientCount--;
+    g_startGame = false;
+
+    closesocket(info->socket);
+    free(info);
+    ExitThread(0);
 }
 
 DWORD WINAPI HandleClient(LPVOID arg)
 {
     ClientInfo* info = (ClientInfo*)arg;
-    printf("Connected from %s:%d\n", inet_ntoa(info->addr.sin_addr), ntohs(info->addr.sin_port));
+    printf("Connected from %s: %d\n", inet_ntoa(info->addr.sin_addr), ntohs(info->addr.sin_port));
 
     char* msg = "Server connection successful!";
     send(info->socket, msg, strlen(msg), 0);
+    
+    // Wait
+    while (true)
+    {
+        if (g_startGame)
+        {
+            printf("%d: Start Game!\n", ntohs(info->addr.sin_port));
+            break;
+        }
+    }
 
-    // TODO: 게임 시작
-
-    // 클라이언트 나가기
-    closesocket(info->socket);
-    free(info);
-    ExitThread(0);
+    PlayGame(info);
+    ExitGame(info);
 }
 
 void Connect()
@@ -103,8 +192,6 @@ void Connect()
     p.myTurn = false;
     p.score = 0;
 
-    int clientCount = 0;
-
     while (true)
     {
         client_socket = accept(g_server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
@@ -115,7 +202,7 @@ void Connect()
         }
 
         // 인원 두 명으로 제한
-        if (clientCount >= MAX_CLIENTS)
+        if (g_clientCount >= MAX_CLIENTS)
         {
             char* msg = "Sorry! Server full";
             send(client_socket, msg, strlen(msg), 0);
@@ -133,7 +220,7 @@ void Connect()
         HANDLE client_thread_id = CreateThread(NULL, 0, HandleClient, ci, 0, &threadId);
         if (client_thread_id == NULL)
         {
-            perror("pthread_create() failed");
+            perror("CreateThread() failed");
 
             closesocket(client_socket);
             CloseHandle(client_thread_id);
@@ -142,7 +229,7 @@ void Connect()
         }
 
         CloseHandle(client_thread_id);
-        g_clients[clientCount++] = ci;
+        g_clients[g_clientCount++] = ci;
     }
 }
 
